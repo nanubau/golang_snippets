e047212177755bef35b6cacb2185a7ab33c1d473
Purav Shah <puravshah@MacBook-Pro.local>
2022-11-20 16:52:39 +0530

    Adding file to write hello world on std output

---
+++ golang_snippets/hello_world.go
@@ -0,0 +1,11 @@
+// First line describes about the name of the package where code is usually `main` 
+package main
+
+// importing the other other packages to use the functionality from the particular packages
+import "fmt"
+
+
+func main(){
+	// Println is function inside fmt package to write data on standard output
+	fmt.Println("Hello World!")
+}
